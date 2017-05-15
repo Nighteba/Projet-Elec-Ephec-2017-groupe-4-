314f5b1ab611893bf26b76653b9eda488493ad77
Nomade73 <cedric.lambin@gmail.com>
2017-05-15 02:50:47 +0200

    Add files via upload

--- Proteus/main.c
+++ Proteus/main.c
@@ -1,70 +1,86 @@
-//Created by Jordan Lagache, Jonathan Noel
+#include <main.h>
 
+#include <stdlib.h>
 #include <string.h>
-//#fuses HS, NOPROTECT, NOLVP, NOWDT
+// No watchdog, no protect, parameter for serial_isr
+#fuses HS, NOPROTECT, NOLVP, NOWDT
+//PIC CLOCK: 20MHz, MUST BE the same for CRYSTAL clock
+#use delay(clock=20000000)
+//Parameter for rs232, synchronized with physical parameters
 #use rs232(baud=9600,xmit=PIN_C6,rcv=PIN_C7,bits=8)
-#use delay(clock=4000000) //Freq du PIC: 4MHz
-/*
-#BYTE PORT_B_PORT=0x81
-#BIT PORT_B_BIT0=PORT_B_PORT.0
-#BIT PORT_B_BIT1=PORT_B_PORT.1 
-*/
 void alert(int temp, int limit);
-void get_temp();
 int toDCB(int val);
-int temp;
+int temp; // temperature value
+int limit=35; //limit default temperature value 
+char  buffer[4]; //used for serial_isr
+int j = 0; //used for serial_isr
+/* INTERRUPTION RECEIVE DATA AVAILABLE (RDA) BLOCK*/
+#INT_RDA
+/*
+This function has been created by the group of Stefan Gotowski, we re-used it, they agreed.
+This function is called when an interruption is sent to TX PIN with DATA AVAILABLE.
+*/
+
+//code inspiré de Jordan Lagache et Jonathan Noel
+void serial_isr(void){
+   char src;
+   char * pEnd;
+   char value;
+   value = getc();
+   if(value == ';'){
+      if(j==1){strcpy(src,"0"+buffer);}
+      else{strcpy(src, buffer);}
+      limit = strtol(buffer,&pEnd,10);
+      j = 0;
+      memset(&buffer[0], 0, sizeof(buffer));
+   }else{
+      buffer[j] = value;
+      j++;
+   }
+}
 void main()
 {
-   setup_adc_ports(AN0); //AN0
+   //ANALOGIC PORT && settings
+   setup_adc_ports(AN0); 
    setup_adc(ADC_CLOCK_DIV_32);
    set_adc_channel(0);
-   enable_interrupts(INT_RDA); //INT_RDA --> PROVOQUE ERREUR
-   enable_interrupts(GLOBAL);
-
-   int limit = 35;
-   int dcb_temp;
+   //INTERRUPTIONS declarations
+   ENABLE_INTERRUPTS(INT_RDA);
+   ENABLE_INTERRUPTS(GLOBAL);
+   int dcb_temp; //converted temperature value
    while(TRUE)
    {
       delay_ms(500);
       int dcb_temp = toDCB((read_adc()*100/1024));
       int temp = (read_adc()*100/1024);
       set_adc_channel(0);
-      output_d(dcb_temp); //dcb_temp
-      printf("%d",temp);
-      alert(temp,limit);
-      //get_temp();
+      output_d(dcb_temp); //Display on 7seg displayers
+      printf("%d\n",temp);//display temperature
+      alert(temp,limit); 
+      delay_ms(500);
    }
 }
-//Permet d'allumer une led en fonction de l'Ã©tat dans laquelle on se trouve.
-void alert(int temp, int limit){
-   if(temp >= limit){ //Etat d'alerte, tempÃ©rature trop Ã©levÃ©
-         output_bit(PIN_B1, 1);
-         output_bit(PIN_B0, 0);
+//Permet d'allumer une led en fonction de l'état dans laquelle on se trouve.
+/*
+Is used in order to light an alert LED if the temperature is too high and
+to light a OK LED if the temperature is under the limit.
+B1 --> Alert LED on PIN_B1
+B0 --> OK LED on PIN_B0
+*/
+void alert(int temp, int limit){ 
+   if(temp >= limit){ //Alert state
+         output_bit(PIN_B1, 1); //Send a high voltage on PIN_B1
+         output_bit(PIN_B0, 0); //low voltage on B0
       }
-      else { //Etat normal, pas de problÃ¨me.
-         output_bit(PIN_B0, 1);
-         output_bit(PIN_B1, 0);
+      else { //Etat normal, pas de problème.
+         output_bit(PIN_B0, 1); //High voltage on B0
+         output_bit(PIN_B1, 0);//Low voltage on B1
       }
 }
+/*
+This function convert binary value to DCB in order to send them to PIN_Dx ports.
+*/
 int toDCB(int val){
    return (((val / 10) % 10) <<4) + (val % 10);
 }
-void get_temp(){
-   char * pEnd;
-   char src, value, bufferChar[4];
-   int j = 0;
-   value = getc();
-   if(value == ';'){
-      if(j==1){strcpy(src,"0"+bufferChar);}
-      else{strcpy(src, bufferChar);}
-      temp = strtol(bufferChar,&pEnd,10);
-      j = 0;
-      //Clear buff
-      memset(&bufferChar[0], 0, sizeof(bufferChar));
-      printf("%c",bufferChar);
-   
-   }else{
-      bufferChar[j] = value;
-      j++;
-   }
-}
+
